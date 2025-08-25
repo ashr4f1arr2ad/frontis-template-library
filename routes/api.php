a236eb2a441c82cfa18aa24f5c3f49a82fc2be00
Ashrafi <ashrafi@Ashrafis-Mac-mini.local>
2025-08-25 18:07:34 +0600

    add: retrieve data to api

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/sites', [SiteController::class, 'index']);
 
 // Create user from wordpress site
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']);
