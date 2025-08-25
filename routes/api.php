f721d37ff4a7d3935327380bd65915b77b2ce7d4
EnamulHaq <mdenamulhaq6263@gmail.com>
2025-08-25 20:15:48 +0600

    Added user with subscritpion with custom middleware with update existing subscription

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UpdateSubscription;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,5 +10,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // Create user from wordpress site
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register'])->middleware('secret.key');
+Route::post('/login', [AuthController::class, 'login'])->middleware('secret.key');;
+Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('secret.key');
+
+// Update subscription for the user
+Route::post('/update', [UpdateSubscription::class, 'update'])->middleware('secret.key');
