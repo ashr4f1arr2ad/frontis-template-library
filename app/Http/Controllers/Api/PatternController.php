76610f37dde07d45477143bf5be0f40164ca151f
Ashrafi <ashrafi@Ashrafis-Mac-mini.local>
2025-09-29 15:37:56 +0600

    fix: pattern saved items

--- app/Http/Controllers/Api/PatternController.php
+++ app/Http/Controllers/Api/PatternController.php
@@ -64,7 +64,7 @@ class PatternController extends Controller
                 $query = Cloud::where('user_id', $user->id)->where('item_type', 'patterns');
 
                 if ($sub_user->id === $user->id) {
-                    $query->whereNull('sub_user'); // Main user
+                    $query->whereNull('sub_user')->orWhere('sub_user', $user->id); // Main user
                 } else {
                     $query->where('sub_user', $sub_user->id); // Sub-user
                 }
