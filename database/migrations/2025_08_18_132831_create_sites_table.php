219e96cf2faf8e63ad02137f87adf10019c9c87e
Ashrafi Ar Raad <developerashrafi7@gmail.com>
2025-10-13 13:53:07 +0600

    fix: json to strings

--- database/migrations/2025_08_18_132831_create_sites_table.php
+++ database/migrations/2025_08_18_132831_create_sites_table.php
@@ -20,10 +20,10 @@ return new class extends Migration
             $table->text('description');
             $table->string('image')->nullable();
             $table->json('dependencies');
-            $table->json('colors');
-            $table->json('color_gradients');
-            $table->json('typographies');
-            $table->json('custom_typographies');
+            $table->string('colors');
+            $table->string('color_gradients');
+            $table->string('typographies');
+            $table->string('custom_typographies');
             $table->json('pages');
             $table->boolean('is_premium')->default(false);
             $table->timestamps();
