fa3d8890b77ae94e4ff487d8ee2f6a0597b158bc
Ashrafi Ar Raad <developerashrafi7@gmail.com>
2025-10-14 13:14:31 +0600

    change: required

--- app/Filament/SuperAdmin/Resources/Pages/Schemas/PagesForm.php
+++ app/Filament/SuperAdmin/Resources/Pages/Schemas/PagesForm.php
@@ -31,7 +31,6 @@ class PagesForm
                     ->label('Slug')
                     ->readOnly(),
                 Textarea::make('content')
-                    ->required()
                     ->label('Content'),
                 Repeater::make('dependencies')
                     ->schema([
