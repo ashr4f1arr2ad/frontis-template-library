e57f1d1c0ef05a1dddf1e5c59a122dfa6b301980
Ashrafi Ar Raad <developerashrafi7@gmail.com>
2026-03-11 09:59:41 +0600

    fix: remove var dump

--- app/Http/Controllers/Api/ResourcesController.php
+++ app/Http/Controllers/Api/ResourcesController.php
@@ -356,9 +356,6 @@ class ResourcesController extends Controller
 
         $data = $validator->validated();
 
-        var_dump($data['pages']);
-        die();
-
         $site = Site::where('slug', $data['slug'])->first();
 
         if (!$site) {
